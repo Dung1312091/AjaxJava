0aad6bcc3fb73837ff2f2ae6415a4bc3dad9f090
Dung1312091 <qddeveloper100194@gmail.com>
2017-06-04 00:18:40 +0700

    xu ly thu ngay thang nam

--- demo/src/controller/ajaxshowdata.java
+++ demo/src/controller/ajaxshowdata.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -27,7 +33,37 @@ public class ajaxshowdata extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    public String FomatDate(String s){
+    	if(s.length()>0){
+    		String year = s.substring(0, 4);
+    		String month = s.substring(4,6 );
+    		String day = s.substring(6,8);
+    		s = year +"/" + month + "/" + day;
+    	}
+    	else{
+    		s = "";
+    	}
+    	return s;
+    }
+    public String FinDayofWeek(String s){
+    	String dayOfWeek = "";
+    	if(s.length()>0){
+    		int year = Integer.parseInt(s.substring(0, 4));
+        	int month = Integer.parseInt(s.substring(4, 6));
+        	int day = Integer.parseInt(s.substring(6, 8));
+        	String dateString = String.format("%d-%d-%d", year, month, day);
+        	Date date;
+        	
+    		try {
+    			date = new SimpleDateFormat("yyyy-M-d").parse(dateString);
+    			dayOfWeek = new SimpleDateFormat("EEEE", Locale.ENGLISH).format(date); 
+    		} catch (ParseException e1) {
+    			// TODO Auto-generated catch block
+    			e1.printStackTrace();
+    		}
+    	}  	
+    	return dayOfWeek;
+    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -35,7 +71,7 @@ public class ajaxshowdata extends HttpServlet {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -45,9 +81,13 @@ public class ajaxshowdata extends HttpServlet {
 		database db = new database();
 		String x = request.getParameter("tintuc");
 		ResultSet rs =null;
+		
 		try {
 			rs = db.getlisttintuc("Select * from tintuc where idloaitin = "+x);
 			while(rs.next()){
+				String date = FomatDate(rs.getString("ngaythang"));
+				String dayofweek = FinDayofWeek(rs.getString("ngaythang"));
+				System.out.println(dayofweek);
 				String note ="";
 				if(Integer.parseInt(rs.getString("flags"))==1){
 				note = "Yes";
@@ -55,10 +95,11 @@ public class ajaxshowdata extends HttpServlet {
 				else{
 					 note = "<a href="+ rs.getString("id")+ ">No</a>";
 				}
+				
 				out.write("<tr>"
 						+ "<td>"+ rs.getString("id") +"</td>"
 						+ "<td>"+ rs.getString("ten") +"</td>"
-						+ "<td>"+ rs.getString("ngaythang") +"</td>"
+						+ "<td>"+dayofweek +"-"+"</br>" + date +"</td>"
 						+ "<td>"+ rs.getString("giobatdau") +"</td>"
 						+ "<td>"+ rs.getString("gioketthuc") +"</td>"
 						+ "<td>"+ note +"</td>"
